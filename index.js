38e95307e3688efc859488d4249044e48b609874
SODUSOLA <odusolasemilore@gmail.com>
2025-05-11 04:44:54 +0100

    Fixed Express import and added Welcome message

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Import Express
 const express = require('express');
-
 // Create the app
 const app = express();
 
-// Define the port
-const PORT = 3000;
+
+// Homepage route with simple welcome message
+app.get('/', (_, res) => {
+    res.send(`
+    <h1>Calculator API</h1>
+    <p>Usage: Calculate Numerical Operations</p>
+    <p>Supported operations: add, subtract, multiply, divide</p>
+    `);
+});
+
 
 // Start the server
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
